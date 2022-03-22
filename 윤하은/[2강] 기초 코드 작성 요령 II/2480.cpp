541c4857258323c9656273c11f90449cb6da89c1
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2022-03-23 08:15:02 +0900

    Update 2480.cpp

--- 윤하은/[2강] 기초 코드 작성 요령 II/2480.cpp
+++ 윤하은/[2강] 기초 코드 작성 요령 II/2480.cpp
@@ -1,5 +1,5 @@
 //220322
-//¹éÁØ 2480¹ø ÁÖ»çÀ§ ¼¼°³ ¹®Á¦ (ºê·ĞÁî 4)
+//ë°±ì¤€ 2480ë²ˆ ì£¼ì‚¬ìœ„ ì„¸ê°œ ë¬¸ì œ (ë¸Œë¡ ì¦ˆ 4)
 //https://www.acmicpc.net/problem/2480
 
 #include <iostream>
@@ -8,32 +8,31 @@ using namespace std;
 int calculateReward(int a, int b, int c)
 {
     int reward = 0;
-
-    //°°Àº ´«ÀÌ 3°³ ³ª¿À´Â °æ¿ì
-    if (a == b && b == c)
-        reward = 10000 + a * 1000;
-
-    //°°Àº ´«ÀÌ 2°³ ³ª¿À´Â °æ¿ì
-    else if (a == b || b == c || a == c)
+    
+    //ê°™ì€ ëˆˆì´ 3ê°œ ë‚˜ì˜¤ëŠ” ê²½ìš°
+    if(a == b && b == c)
+        reward = 10000 + a*1000;
+    
+    //ëª¨ë‘ ë‹¤ë¥¸ ëˆˆì´ ë‚˜ì˜¤ëŠ” ê²½ìš°
+    else if(a != b && b!=c && c!= a)
+        reward = max({a, b, c})*100;
+    
+    //ê°™ì€ ëˆˆì´ 2ê°œ ë‚˜ì˜¤ëŠ” ê²½ìš°
+    else
     {
-        if (a == b || a == c)
+        if(a == b || a == c)
             reward = 1000 + a * 100;
         else//b == c
             reward = 1000 + b * 100;
     }
-
-    //¸ğµÎ ´Ù¸¥ ´«ÀÌ ³ª¿À´Â °æ¿ì
-    else
-        reward = max({ a, b, c }) * 100;
-
-
+    
     return reward;
-
+    
 }
 int main(void)
 {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     printf("%d", calculateReward(a, b, c));
-
+    
 }
