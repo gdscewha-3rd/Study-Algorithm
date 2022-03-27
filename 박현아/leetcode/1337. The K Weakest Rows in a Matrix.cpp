437bbf9824e5c7835f76d647e1889919b69f1500
hyunaaah <67725652+hak2711@users.noreply.github.com>
2022-03-27 09:38:09 +0900

    Create 1337. The K Weakest Rows in a Matrix.cpp

---
+++ 박현아/leetcode/1337. The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,29 @@
+//220327
+//leetcode #1337 The K Weakest Rows in a Matrix /Easy
+//https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<pair<int,int>> sums;
+        vector<int> results;
+        
+        for (int i = 0; i<mat.size(); i++){
+            sums.push_back(make_pair(accumulate(mat[i].begin(), mat[i].end(), 0), i));
+        }
+        
+        
+        sort(sums.begin(), sums.end());
+        
+        for (int i = 0; i<k; i++){
+            results.push_back(sums[i].second);
+        }
+        
+        return results;
+    }
+};
