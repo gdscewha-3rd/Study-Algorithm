31cb1523a300b09bb78418b615e0c10acfd76cbd
unknown <ayeon3052@ewhain.net>
2022-03-29 01:47:13 +0900

    [boj] 22.03.29 1475 improve

--- 장아연/week2/1475.py
+++ 장아연/week2/1475.py
@@ -2,14 +2,17 @@
 # 문제 백준 1475 : https://www.acmicpc.net/problem/1475
 
 N = input()
-arr = [0]*9
+arr = [0]*10
 # print(arr)
 
 for i in N:
     if (int(i) == 9) or (int(i) == 6):
-        arr[6] += 0.5
+        if arr[6] <= arr[9]:
+            arr[6] += 1
+        else:
+            arr[9] += 1
     else:
         arr[int(i)] += 1
-arr[6] = int(arr[6]+0.5)
+
 # print(arr)
 print(max(arr))
