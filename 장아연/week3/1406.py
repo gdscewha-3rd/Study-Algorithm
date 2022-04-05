df8e23dfe49afa704ae18d52e3f562e36f041938
JangAyeon <ayeon3052@ewhain.net>
2022-04-05 09:43:20 +0900

    [boj] 22.04.5 1406 solve

--- 장아연/week3/1406.py
+++ 장아연/week3/1406.py
@@ -1,16 +1,23 @@
-st_l = list(input())
+from sys import stdin
+
+st_l = list(stdin.readline().strip())
 st_r = []
 
 
 for _ in range(int(input())):
-    cmd = list(input().split())
-    if (cmd[0] == "D") and (st_r):
-        st_l.append(st_r.pop())
-    elif cmd[0] == "L" and st_l:
-        st_r.append(st_l.pop())
-    elif cmd[0] == "B" and st_l:
-        st_l.pop()
+    cmd = stdin.readline().strip()
+    # print(cmd)
+    if (cmd[0] == "D"):
+        if (st_r):
+            st_l.append(st_r.pop())
+    elif cmd[0] == "L":
+        if st_l:
+            st_r.append(st_l.pop())
+    elif cmd[0] == "B":
+        if st_l:
+            st_l.pop()
     else:
         st_l.append(cmd[-1])
-
+    # print("###st_r", st_r)
+    # print("###st_l", st_l)
 print("".join(st_l+list(reversed(st_r))))
