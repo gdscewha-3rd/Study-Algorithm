dbd45a27b4d69bbdf2e9ca301aad93109f4c649f
JangAyeon <ayeon3052@ewhain.net>
2022-04-05 10:53:46 +0900

    [boj] 22.04.05 1158 solved

--- 장아연/week3/1158.py
+++ 장아연/week3/1158.py
@@ -5,17 +5,16 @@ N, M = map(int, input().split())
 #print(N, M)
 string = [i+1 for i in range(N)]
 ans = []
+idx = 0
 
 while string:
-    for i in range(M):
-        print(i)
-        w = string[0]
-        string.remove(w)
-        if i == M-1:
-            ans.append(w)
-        else:
-            string.append(w)
-        print("ans: ", ans)
-        print("string : ", string)
+    idx += M-1
+    if idx >= len(string):
+        idx = idx % len(string)
+
+    ans.append(string.pop(idx))
+
+    #print("ans: ", ans)
+    #print("string : ", string)
 
 print("<", ", ".join(str(i) for i in ans)[:], ">", sep="")
