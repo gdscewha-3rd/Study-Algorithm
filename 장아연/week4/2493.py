160b53501e9c8320770f83263708d49d523cd301
JangAyeon <ayeon3052@ewhain.net>
2022-04-13 19:05:53 +0900

    [boj] 22.04.13 2493 review

--- 장아연/week4/2493.py
+++ 장아연/week4/2493.py
@@ -1,14 +1,19 @@
+# https://www.acmicpc.net/problem/2493
+
 import sys
 input = sys.stdin.readline
-n = int(input())
-arr = list(map(int, input().split()))
-stack = []  # 수신 가능한 탑
-res = [0] * n
 
-for i in range(n):
+N = int(input())
+arr = list(map(int, input().split()))
+# print(arr)
+stack = []
+res = [0]*N
+
+for i in range(N):
     while stack and arr[stack[-1]] < arr[i]:
-        stack.pop()  # 수신 불가능
+        stack.pop()
     if stack:
-        res[i] = stack[-1] + 1
+        res[i] = stack[-1]+1
     stack.append(i)
-print(' '.join(list(map(str, res))))
+
+print(" ".join(map(str, res)))
