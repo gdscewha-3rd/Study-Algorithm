bc055241a34ad8310962941aa01862f37b11c8be
Kim Hyuna <akim.cse@ewhain.net>
2022-05-22 02:38:47 +0900

    [LeetCode] 766

---
+++ 김현아/766.cpp
@@ -0,0 +1,20 @@
+﻿//220522
+//LeetCode 766. Toeplitz Matrix (Easy)
+//https://leetcode.com/problems/toeplitz-matrix/
+
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i - 1][j - 1] != matrix[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
