f84e4607d6160f15623c9f142322c4956e7e1d5c
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2022-06-16 12:21:44 +0900

    [9강] 연습 문제

---
+++ 윤하은/[9강] BFS/7569.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+int box[101][101][101];
+int dist[101][101][101];
+
+//확인 순서 : 상하좌우(2차원기준)위층아래층
+int dx[6] = {-1, 1, 0, 0, 0, 0};
+int dy[6] = {0, 0, -1, 1, 0, 0};
+int dz[6] = {0, 0, 0, 0, -1, 1};
+int main(void)
+{
+    //ios::sync_with_stdio(false);
+    //cin.tie(NULL);
+    
+    int row, col, h;
+    cin >> row >> col >> h;
+    
+    queue<tuple<int, int, int>> q;
+    
+    for(int i = 0; i<h; i++)
+    {
+        for(int j = 0; j<row; j++)
+        {
+            for(int k = 0; k<col; k++)
+            {
+                cin >> box[i][j][k];
+                //익은 토마토일 경우
+                if(box[i][j][k] == 1)
+                {
+                    q.push({i, j, k});
+                    //방문여부 확인을 위해 1부터 시작 -> 결과 출력할때 1빼고 출력
+                    dist[i][j][k] = 1;
+                }
+            }
+        }
+    }
+    
+    printf("check\n");
+    //처음부터 모두 익은 토마토인지 확인해주기
+    bool isAlreadyRipen = true;
+    for(int i = 0; i<h; i++)
+    {
+        for(int j = 0; j<row; j++)
+        {
+            for(int k =0; k<col; k++)
+            {
+                if(box[i][j][k])
+                    isAlreadyRipen = false;
+            }
+        }
+    }
+    if(isAlreadyRipen)
+    {
+        cout << "0";
+        return 0;
+    }
+    printf("check\n");
+    
+    while(!q.empty())
+    {
+        tuple<int, int, int> cur = q.front();
+        q.pop();
+        
+        printf("check\n");
+        for(int i = 0; i<6; i++)
+        {
+            int nz = get<0>(cur) + dz[i];
+            int nx = get<1>(cur) + dx[i];
+            int ny = get<2>(cur) + dy[i];
+            
+            //토마토가 들어있지 않은 칸일 경우
+            if(box[nz][nx][ny] == -1) continue;
+            //이미 방문한 칸일 경우
+            if(dist[nz][nx][ny] != 0) continue;
+            //outofindex
+            if(nz < 0 || nz >= h || nx <0 || nx >= row || ny < 0 || ny >= col) continue;
+            
+            q.push({nz, nx, ny});
+            dist[nz][nx][ny] = dist[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
+        }
+    }
+    
+    cout << "bfs fin\n";
+    //토마토가 모두 익지 못하는 경우 체크 dist사용
+    int result = 0;
+    printf("check\n");
+    for(int i =0; i<h; i++)
+    {
+        for(int j = 0; j<row; j++)
+        {
+            for(int k =0; k<col; k++)
+            {
+                if(box[i][j][k] != -1 && dist[i][j][k] < 1)
+                {
+                    cout << "-1";
+                    return 0;
+                }
+                if(dist[i][j][k] > result)
+                    result = dist[i][j][k];
+            }
+        }
+    }
+    printf("check\n");
+    cout << result-1;
+}
