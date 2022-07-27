3e50d07743573d95597e4fe78b37d0f6a1d527b4
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2022-07-28 01:34:24 +0900

    [2강] 파일 수정

---
+++ 윤하은/[2강] 기초 코드 작성 요령 II/1000.cpp
@@ -0,0 +1,13 @@
+//220319
+//https://www.acmicpc.net/problem/1000
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = 0, b = 0;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+
+}
